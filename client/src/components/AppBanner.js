e30d3f81332e855017b83f312622b0dec175cd57
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-01 22:04:32 -0400

    Added AppBanner and a button
    Banner is not complete, added button for backend tests, no functionality atm.

---
+++ client/src/components/AppBanner.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import {navbar, nav, button} from 'react-bootstrap'
+
+export default function AppBanner() {
+    return(
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button type="button" class="btn btn-primary">Click Here (Test)</button>
+        </nav>
+    )
+}
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import AppBanner from './AppBanner'
+
+export { 
+    AppBanner,  
+ }
