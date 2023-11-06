22320896f3ab183d9b0e5f2b5b36ba1551e42e81
darrenwin321 <89400271+darrenwin321@users.noreply.github.com>
2023-11-06 01:54:34 -0500

    added server.js

--- client/src/components/TestingComponent.js
+++ client/src/components/TestingComponent.js
@@ -5,48 +5,63 @@ import api from "../store/store-request-api"
 
 export default function TestingComponent() {
     const [testStrings, setTestStrings] = useState([]);
-    const [stringInput, setStringInput] = useState({testString: ""});
-    const baseUrl = "http://geocraftmaps.azurewebsites.net";
+    const [stringInput, setStringInput] = useState([]);
+    const baseUrl = "https://geocraftmaps.azurewebsites.net";
  
-    useEffect(() => {
-        let tests = []
-        async function asyncGetTests() {
-            console.log('useEffect GET TESTS')
-            const response = await api.getTests();
-            console.log("After getTests()")
-            if (response.data.success) {
-                console.log("got test strings from mongo")
-                tests = response.data.testString;
-            }
-            else {
-                console.log("COULDNT GET TEST STRINGS")
-            }
+    // useEffect(() => {
+    //     let tests = []
+    //     async function asyncGetTests() {
+    //         console.log('useEffect GET TESTS')
+    //         const response = await api.getTests();
+    //         console.log("After getTests()")
+    //         if (response.data.success) {
+    //             console.log("got test strings from mongo")
+    //             tests = response.data.testString;
+    //         }
+    //         else {
+    //             console.log("COULDNT GET TEST STRINGS")
+    //         }
 
-            setTestStrings(tests);
-        }
-        asyncGetTests();
-    });
+    //         setTestStrings(tests);
+    //     }
+    //     asyncGetTests();
+    // });
+
+    useEffect(() => {
+        axios
+          .get(`${baseUrl}/get-users`)
+          .then((res) => setStringInput(res.data))
+          .catch((err) => console.log(err));
+      }, []);
 
     const handleChange = (event) => {
         const { value } = event.target;
         setStringInput(value);
     };
 
-    const saveData = (event) => {
-        async function asyncCreateNewTest() {
-            console.log('saveData started')
-            let payload = {testString: stringInput}
-            console.log('payload set: ', payload.testString)
-            const response = await api.createTestString(payload.testString);
-            if (response.data.success) {
-                console.log("test string added to mongo")
-            }
-            else {
-                console.log("COULDNT ADD TEST STRING")
-            }
-        }
-        asyncCreateNewTest();
-    };
+    // const saveData = (event) => {
+    //     async function asyncCreateNewTest() {
+    //         console.log('saveData started')
+    //         let payload = {testString: stringInput}
+    //         console.log('payload set: ', payload.testString)
+    //         const response = await api.createTestString(payload.testString);
+    //         if (response.data.success) {
+    //             console.log("test string added to mongo")
+    //         }
+    //         else {
+    //             console.log("COULDNT ADD TEST STRING")
+    //         }
+    //     }
+    //     asyncCreateNewTest();
+    // };
+    const saveData = (e) => {
+        e.preventDefault();
+    
+        axios
+          .post(`${baseUrl}/create`, user)
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
+      };
 
     // const saveData = (event) => {
     //     event.preventDefault();
@@ -66,7 +81,7 @@ export default function TestingComponent() {
             testStrings.map((test) => {
                 return (
                     <div>
-                        <h3>{test.testString}</h3>
+                        <h3>{stringInput}</h3>
                     </div>
                 );
             })
