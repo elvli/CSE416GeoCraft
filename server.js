295b8db39844c7568ca5ba2b2e8ac63f30d6de12
darrenwin321 <89400271+darrenwin321@users.noreply.github.com>
2023-11-06 02:02:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.post("/create", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+app.use(express.static('./client/build'))
+app.get('*', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 app.listen(port, () => {
   console.log(`Server is running on post ${port}`);
 });
