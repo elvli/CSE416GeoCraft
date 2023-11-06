98e5cbf56483b9313281a939fda1938b3e5f12cf
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-05 19:45:40 -0500

    Update index.js

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'https://mongodb://geocraftmapsdb:FvG00KuXU8a0CrPI4wNX4bpO9ABF3wT76LCcpgpRwZXORQ4I7Zh7jzE6AVctvO5pV6o1y8q8GPFJACDbxBc4OQ==@geocraftmapsdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@geocraftmapsdb@/api',
+    baseURL: 'https://mongodb://geocraftmapsdb:FvG00KuXU8a0CrPI4wNX4bpO9ABF3wT76LCcpgpRwZXORQ4I7Zh7jzE6AVctvO5pV6o1y8q8GPFJACDbxBc4OQ==@geocraftmapsdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@geocraftmapsdb@',
 })
 
 export const createTestString = (testString) => {
