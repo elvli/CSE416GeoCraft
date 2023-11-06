d54d1b417e9c74ffe9fe66bfa05f4ed96e2d02e0
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-06 03:12:39 -0500

    Testing Template

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
+import React from "react";
 
-import React from 'react'
-import { AppBanner, LeftSideBar, TestingComponent} from './components'
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
+const App = () => {
   return (
-    <div className="App">
-      <AppBanner/>
-      {/* <LeftSideBar/> */}
-      <TestingComponent/>
+    <div>
+      <Navbar />
+      <div style={{ margin: 20 }}>
+      <Routes>
+        <Route exact path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- server/routes/test-router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const TestController = require('../controllers/test-controller')
-const router = express.Router()
-
-router.post('/tests', TestController.createTestString)
-router.get('/tests', TestController.getTests)
-
-module.exports = router
