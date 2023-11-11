e72b07738582b1112b49695f456bf238d1334b98
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-10 21:30:33 -0500

    Added Buttons to app banner
    Buttons are not in the proper positions yet

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -1,10 +1,26 @@
 import React from 'react'
-// import {navbar, nav, button} from 'react-bootstrap'
+import '../App.css'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GeoCraftLogo from './Icons/GeoCraftGlobeWhite.png';
+import { Person } from 'react-bootstrap-icons';
+
 
 export default function AppBanner() {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark app-banner">
           <p className="navbar-brand">GeoCraft</p>
+          <Button className="navbar-brand .banner-button btn btn-dark">
+            <img src={GeoCraftLogo} 
+              alt="GeoCraft Logo" 
+              className="banner-logo img-fluid" 
+              style={{ maxHeight: '5vh'}}
+            />
+          </Button>
+
+          <Button className="navbar-brand .banner-button btn btn-dark banner-button">
+            <Person style={{ fontSize: '3vh', float: 'right' }}/>
+          </Button>
         </nav>
     )
 }
