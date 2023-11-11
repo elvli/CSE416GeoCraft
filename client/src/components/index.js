2327672a4b0f0f307d1be378da8a624637bc3a20
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-11 04:02:53 -0500

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,12 +1,10 @@
 import AppBanner from './AppBanner'
 import LeftSideBar from './LeftSideBar'
 import MapBackground from './MapBackground'
-import LoginScreen from './Login/LoginScreen'
 
 
 export { 
     AppBanner,
     LeftSideBar,
     MapBackground,
-    LoginScreen,
 }
