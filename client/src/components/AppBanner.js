181c2a347b6c6e4716b82230d195e668d0ab2049
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-10 22:30:13 -0500

    Added Double click to change name in banner
    The logo still is not centered on the appbanner. I also fixed the pencil icon on the side bar

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -1,26 +1,100 @@
 import React from 'react'
 import '../App.css'
-import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GeoCraftLogo from './Icons/GeoCraftGlobeWhite.png';
+import { useState, useRef } from 'react';
+import { Button, Dropdown} from 'react-bootstrap';
 import { Person } from 'react-bootstrap-icons';
 
 
 export default function AppBanner() {
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark app-banner">
-          <p className="navbar-brand">GeoCraft</p>
-          <Button className="navbar-brand .banner-button btn btn-dark">
-            <img src={GeoCraftLogo} 
-              alt="GeoCraft Logo" 
-              className="banner-logo img-fluid" 
-              style={{ maxHeight: '5vh'}}
-            />
-          </Button>
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState('Map Name Double Click to Edit');
+  const inputRef = useRef();
 
-          <Button className="navbar-brand .banner-button btn btn-dark banner-button">
-            <Person style={{ fontSize: '3vh', float: 'right' }}/>
-          </Button>
-        </nav>
-    )
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleBlur = () => {
+    handleSave();
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSave();
+    }
+  };
+
+  const handleLogout = () => {
+    // Handle logout logic
+    console.log('Logging out...');
+  };
+
+  const handleCreateAccount = () => {
+    // Handle create account logic
+    console.log('Creating a new account...');
+  };
+
+  const handleLogoClick = () => {
+    // Handle create account logic
+    console.log('Clicked the Logo...');
+  };
+
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark app-banner">
+
+      {isEditing ? (
+          <div className="float-left">
+            <input
+              ref={inputRef}
+              type="text"
+              className="form-control"
+              value={text}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyDown}
+            />
+          </div>
+        ) : (
+          <p className="navbar-brand" 
+            onDoubleClick={handleDoubleClick} 
+            style={{ marginTop: '12px', marginLeft: '12px' }}
+          >
+            {text}
+          </p>
+        )}
+
+      <Button 
+        className="navbar-brand .banner-button btn btn-dark mx-auto" 
+        onClick={handleLogoClick}
+      >
+        <img src={GeoCraftLogo}
+          alt="GeoCraft Logo"
+          className="banner-logo img-fluid"
+          style={{ maxHeight: '4vh' }}
+        />
+      </Button>
+
+      <Dropdown className="position-fixed" style={{ right: '0', maxHeight: '4vh', zIndex: 1000 }}>
+        <Dropdown.Toggle variant="dark" id="dropdown-basic">
+          <Person className="fs-4" />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={handleLogout}>Log Out</Dropdown.Item>
+          <Dropdown.Item onClick={handleCreateAccount}>Create New Account</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </nav>
+  )
 }
