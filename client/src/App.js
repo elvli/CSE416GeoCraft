3e90833fcadc4a0a9c5f2ddbdbf0638bebdaae55
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-11 23:37:52 -0500

    Small Formating Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import baseUrl from "./baseUrl";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import { React, useState, useEffect } from "react";
-import { Route, Routes, Outlet, Navigate} from "react-router-dom";
-import { AppBanner, LeftSideBar, RightSideBar, MapBackground, LoginScreen, SignUpScreen, HomeScreen, PasswordReset } from './components'
+import { Route, Routes } from "react-router-dom";
+import { LoginScreen, SignUpScreen, HomeScreen, PasswordReset } from './components'
 
 function App() {
-  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -19,12 +17,12 @@ function App() {
 
   return (
     <div className="App">
-        <Routes>
-          <Route index element={<HomeScreen/>}/>
-          <Route path="sign-up/" element={<SignUpScreen/>}/>
-          <Route path="login/" element={<LoginScreen/>}/>
-          <Route path="password-reset/" element ={<PasswordReset/>}/>
-        </Routes> 
+      <Routes>
+        <Route index element={<HomeScreen />} />
+        <Route path="signup/" element={<SignUpScreen />} />
+        <Route path="login/" element={<LoginScreen />} />
+        <Route path="password-reset/" element={<PasswordReset />} />
+      </Routes>
     </div>
   );
 }
