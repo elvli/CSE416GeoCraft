7793dbb71e3d0f9d1f1aef497a8cebbb00f0b73c
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-13 01:30:13 -0500

    MapCreateModal

---
+++ client/src/components/MapCreateModal/MapCreateModal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./MapCreateModal.scss";
+import AppBanner from "../AppBanner";
+import App from "../../App";
+import Form  from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export default function MapCreateModal () {
+
+    return(
+        <div class="modal" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+        )
+}
