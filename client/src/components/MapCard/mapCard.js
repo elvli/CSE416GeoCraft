cc7a65119ef15c478c591cd93ceb6ca7b8088eda
darrenwin321 <89400271+darrenwin321@users.noreply.github.com>
2023-11-15 01:05:18 -0500

    Left side bar updata
    fixed sort button dropdown so that it is not underneath other elements. Added map cards which change depending on if they are published or not. Added two sample map cards.

--- client/src/components/MapCard/mapCard.js
+++ client/src/components/MapCard/mapCard.js
@@ -1,14 +1,67 @@
 import React, { useState } from 'react'
-import Button from "react-bootstrap/Button";
-import { Trash } from 'react-bootstrap-icons';
-export default function MapCard(){
+import {Button, Dropdown} from "react-bootstrap";
+import { Trash, HandThumbsUpFill, HandThumbsDownFill, ThreeDotsVertical, PenFill } from 'react-bootstrap-icons';
+import './MapCard.scss'
+export default function MapCard(props){
+    const { map } = props
     let icon = "apples"
     async function handleDelete(event) {
         document.getElementById("map-create-modal").classList.add("is-visible")
     }
+    let dropdown = <div className='options-button'>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                <ThreeDotsVertical />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className='dropdown-menu'>
+                                <Dropdown.Item className='options-button-options'>Fork</Dropdown.Item>
+                                <Dropdown.Item className='options-button-options'>Delete</Dropdown.Item>
+                                <Dropdown.Item className='options-button-options'>Rename</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+
+    let others = <div className='d-flex flex-row-reverse'>
+                    <Button className='btn btn-light dislike-button'><PenFill/></Button>
+                 </div>
+    if (map.published){
+        dropdown = <div className='options-button'>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                <ThreeDotsVertical />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className='dropdown-menu'>
+                                <Dropdown.Item className='options-button-options'>Fork</Dropdown.Item>
+                                <Dropdown.Item className='options-button-options'>Delete</Dropdown.Item>
+                                <Dropdown.Item className='options-button-options'>Export</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+
+        others = <div className='d-flex flex-row-reverse'>
+                        <Button className='btn btn-light dislike-button'><HandThumbsDownFill/> {map.dislikes.length}</Button>
+                        <Button className='btn btn-light like-button'><HandThumbsUpFill/> {map.likes.length}</Button>
+                     </div>
+    }
 
     return(
-        <Button type="button" onClick={handleDelete}><Trash/></Button>
+        <div>
+            <div className="card map-card">
+                <div className="card-header">
+                    <p className="username">{map.title}</p>
+                    {dropdown}
+                    
+                </div>
+
+                <div className="card-body" >
+                    <p className="card-text comment-text">By: {map.author}</p>
+                    {others}
+                    
+                </div>
+            </div>
+        </div>
+        
+        
     )
 
 }
