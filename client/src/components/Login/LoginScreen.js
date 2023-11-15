8eb16ea4fe96c9d974a3011f163e7792795922bf
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-14 23:29:21 -0500

    Added Logos and some css changes

--- client/src/components/Login/LoginScreen.js
+++ client/src/components/Login/LoginScreen.js
@@ -1,40 +1,39 @@
 import React from "react";
 import "./LoginScreen.scss";
 import AppBanner from "../AppBanner/AppBanner";
-import App from "../../App";
-import Form  from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import GeoCraftLogoBlack from '../Icons/GeoCraftLogoBlack.png'
 
-export default function LoginScreen () {
-    return(
+export default function LoginScreen() {
+    return (
         <div className="login-screen">
             <div>
-                <AppBanner/>
+                <AppBanner />
             </div>
+            <img src={GeoCraftLogoBlack} className="sign-in-logo"/>
             <div>
                 <Form className="login-credentials">
                     <h1 className="heading">Sign In</h1>
-                    <br/>
-                    <br/>
+                    <br />
                     <Form.Group>
-                        <Form.Control className="form-items" type="email" placeholder="Email" size="lg"/>
+                        <Form.Control className="form-items" type="email" placeholder="Email" size="lg" />
                     </Form.Group>
-                    <br/>
+                    <br />
                     <Form.Group>
-                        <Form.Control className="form-items"  type="password" placeholder="Password" size="lg"/>
+                        <Form.Control className="form-items" type="password" placeholder="Password" size="lg" />
                     </Form.Group>
-                    <br/>
+                    <br />
                     <div className="register">
                         <div className="register-items">
-                            <a href="#!">Trouble Signing in? Click here.</a>
+                            <a href="/password-reset">Trouble Signing in? Click here.</a>
                         </div>
-                        <div className= "register-items">
-                            <a href="#!">Sign Up</a>
+                        <div className="register-items">
+                            <a href="/sign-up">Sign Up</a>
                         </div>
-                        
-                        
+
                     </div>
-                    <br/>
+                    <br />
                     <Form.Group>
                         <Button className="form-button" type="submit">Sign In</Button>
                     </Form.Group>
