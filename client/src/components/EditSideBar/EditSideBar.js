d18ae939b7aadd1ed42698a806a0035c0a09dd0e
Alan Thai <alanthai02@gmail.com>
2023-11-15 09:45:38 -0500

    Added table to heatmap

--- client/src/components/EditSideBar/EditSideBar.js
+++ client/src/components/EditSideBar/EditSideBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 import { Gear, ViewStacked, PencilSquare, Wrench } from 'react-bootstrap-icons';
 import './EditSideBar.scss'
 import Accordion from 'react-bootstrap/Accordion';
@@ -10,10 +10,61 @@ import SaveAndExitModal from '../SaveAndExitModal/SaveAndExitModal'
 export default function EditSideBar(props) {
   const [isToggled, setIsToggled] = useState(false);
   const [show, setShow] = useState(false);
+  const [isEditing, setIsEditing] = useState(null);
+  const [isEditingHeader, setIsEditingHeader] = useState(null)
+  const [tableData, setTableData] = useState([
+    { id: 1, xCoordinate: 0.5, yCoordinate: 0.7 },
+    { id: 2, xCoordinate: 0.3, yCoordinate: 0.2 },
+    { id: 3, xCoordinate: 0.8, yCoordinate: 0.4 },
+    { id: 4, xCoordinate: 0.2, yCoordinate: 0.9 },
+    { id: 5, xCoordinate: 0.6, yCoordinate: 0.1 },
+  ]);
+  const [tableHeaders, setTableHeaders] = useState([
+    'ID', 'X Coordinate', 'Y Coordinate'
+  ]);
   function toggleSideBar(event) {
     event.preventDefault();
     setIsToggled(!isToggled);
   }
+  const handleHeaderDoubleClick = (index) => {
+    setIsEditingHeader(index);
+  };
+
+  const handleHeaderChange = (event, index) => {
+    const updatedHeaders = [...tableHeaders];
+    updatedHeaders[index] = event.target.value;
+    setTableHeaders(updatedHeaders);
+  };
+
+  const handleHeaderBlur = () => {
+    setIsEditingHeader(null);
+  };
+  
+  const handleDoubleClick = (rowIndex, colName) => {
+    setIsEditing({ rowIndex, colName });
+  };
+
+  const handleEditChange = (event, rowIndex, colName) => {
+    const updatedData = tableData.map((row, index) => {
+      if (index === rowIndex) {
+        return { ...row, [colName]: event.target.value };
+      }
+      return row;
+    });
+    setTableData(updatedData);
+  };
+
+  const handleEditBlur = () => {
+    setIsEditing(null);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleEditBlur();
+      handleHeaderBlur();
+    }
+  };
+
 
   return (
     <div>
@@ -51,9 +102,7 @@ export default function EditSideBar(props) {
         </div>
         <div className={`bg-light border-right ${isToggled ? 'invisible' : 'visible'}`} id="left-sidebar-wrapper">
           <div className="list-group list-group-flush tools-list">
-
             <div className="row">
-
               <Accordion defaultActiveKey={['0']} alwaysOpen>
                 <Accordion.Item eventKey="0">
                   <Accordion.Header>Attach Data</Accordion.Header>
@@ -64,7 +113,57 @@ export default function EditSideBar(props) {
                 <Accordion.Item eventKey="1">
                   <Accordion.Header>Heat Map Data</Accordion.Header>
                   <Accordion.Body className="d-flex justify-content-between">
-                    Heat Map Stuff
+                  <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          {tableHeaders.map((header, index) => (
+                            <th
+                              key={index + 1}
+                              onDoubleClick={() => handleHeaderDoubleClick(index + 1)}
+                              onBlur={handleHeaderBlur}
+                            >
+                              {isEditingHeader === index + 1 ? (
+                                <input
+                                  type="text"
+                                  value={header}
+                                  onChange={(event) => handleHeaderChange(event, index + 1)}
+                                  onKeyPress={handleKeyPress}
+                                />
+                              ) : (
+                                header
+                              )}
+                            </th>
+                          ))}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {tableData.map((row, rowIndex) => (
+                          <tr key={row.id}>
+                            {Object.keys(row).map((colName, colIndex) => (
+                              <td
+                                key={colIndex}
+                                onDoubleClick={() => handleDoubleClick(rowIndex, colName)}
+                                onBlur={handleEditBlur}
+                              >
+                                {isEditing &&
+                                isEditing.rowIndex === rowIndex &&
+                                isEditing.colName === colName ? (
+                                  <input
+                                    type="text"
+                                    value={row[colName]}
+                                    onChange={(event) => handleEditChange(event, rowIndex, colName)}
+                                    onBlur={handleEditBlur}
+                                    onKeyPress={handleKeyPress}
+                                  />
+                                ) : (
+                                  row[colName]
+                                )}
+                              </td>
+                            ))}
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
                   </Accordion.Body>
                 </Accordion.Item>
               </Accordion>
