b683d635689b0e2ad3bdb3105f5be8db2cf68b48
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-17 23:31:07 -0500

    Added spacer in EditScreen
    Since the app banner is not fixed to the top of the screen, some of the elements are rendered underneath the app banner, causing some buttons to be inaccessible. The spacer is just a div that takes up the same space the app banner did originally.

--- client/src/components/EditScreen/EditScreen.js
+++ client/src/components/EditScreen/EditScreen.js
@@ -4,23 +4,24 @@ import AppBanner from "../AppBanner/AppBanner";
 import { MapBackground } from "..";
 import EditSideBar from "../EditSideBar/EditSideBar";
 import { useState } from "react";
-export default function EditScreen () {
-    return(
-        <div>
-            <AppBanner />
-            <div className="row1">
+export default function EditScreen() {
+  return (
+    <div>
+      <div style={{ height: '5vh' }} />
+      <AppBanner />
+      <div className="row1">
 
-                <div className="background">
-                    <MapBackground />
-                </div>
-
-                <div className="foreground">
-
-                    <EditSideBar/>
-                </div>
-
-            </div>
-            {/* <button onClick={() => navigate("create")}>Create</button> */}
+        <div className="background">
+          <MapBackground />
         </div>
-    )
+
+        <div className="foreground">
+
+          <EditSideBar />
+        </div>
+
+      </div>
+      {/* <button onClick={() => navigate("create")}>Create</button> */}
+    </div>
+  )
 }
