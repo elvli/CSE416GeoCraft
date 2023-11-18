05e98bd9e82db530844bda42bbcd06e990d06794
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-18 16:42:09 -0500

    revert

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const path = require('path')
+const AuthController = require('./controllers/auth-controller')
+
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// app.use(express.urlencoded({ extended: true }))
+// app.use(cors({
+//     origin: ["http://localhost:3000"],
+//     credentials: true
+// }))
+
+// http://localhost:3001
+// https://geocraftmaps.azurewebsites.net
+// https://geocraftserver.azurewebsites.net/
+app.use(cors({
+    origin: ["https://geocraftmaps.azurewebsites.net"],
+    credentials: true
+}))
+app.use(express.json());
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DATABASE IS CONNECTED..."))
+  .catch((err) => console.log(err));
+
+// Auth routes
+// const authRouter = require('./routes/auth-router')
+// app.use('/auth', authRouter)
+app.get('/loggedIn', AuthController.getLoggedIn)
+app.post('/login', AuthController.loginUser)
+app.get('/logout', AuthController.logoutUser)
+app.post('/register', AuthController.registerUser)
+// app.get("/get-users", (req, res) => {
+//   User.find()
+//     .then((users) => res.json(users))
+//     .catch((err) => console.log(err));
+// });
+
+// app.post("/create", (req, res) => {
+//   //save to mongodb and send response
+//   const newUser = new User({
+//     name: req.body.name,
+//     lastName: req.body.lastName,
+//   });
+
+//   newUser
+//     .save()
+//     .then((user) => res.json(user))
+//     .catch((err) => console.log(err));
+// });
+app.use(express.static('./client/build'))
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on post ${PORT}`);
+});
