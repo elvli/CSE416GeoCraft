ea9510532d10d84d632e4ac427f4f558ca750565
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-18 16:28:10 -0500

    removed 10000 from baseurl

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://geocraft-backend.onrender.com:10000";
+const baseUrl = "https://geocraft-backend.onrender.com";
 // https://geocraftserver.azurewebsites.net
 //http://localhost:3001
 // https://geocraftmaps.azurewebsites.net
