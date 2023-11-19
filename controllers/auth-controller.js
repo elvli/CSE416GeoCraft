85ea076c6b0b2cb03d83f61f88ee9625c608bce1
darrenwin321 <89400271+darrenwin321@users.noreply.github.com>
2023-11-19 16:28:13 -0500

    changed to bcryptjs

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,6 @@
 const auth = require('../auth')
 const User = require('../models/user-model')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 getLoggedIn = async (req, res) => {
     try {
