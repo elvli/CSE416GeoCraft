19be08a110541eeccfd8dfe6367b3a8cebb4f7bc
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-18 20:09:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://geocraftmaps.azurewebsites.net');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
