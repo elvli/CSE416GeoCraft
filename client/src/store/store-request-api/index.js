aad99a3c6e6a30156ed5f2f23c479a0815d13a44
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-19 20:29:01 -0500

    Frontend Store routes

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -1,8 +1,49 @@
-// import axios from 'axios';
-// axios.defaults.withCredentials = true;
-// const api = axios.create({
-//     baseURL: 'https://geocraftserver.azurewebsites.net/',
-// https://geocraftmaps.azurewebsites.net
-// })
+import axios from 'axios'
+axios.defaults.withCredentials = true;
+const api = axios.create({
+    baseURL: 'https://geocraftmapsbackend.onrender.com',
+})
 
-// export const createUser = ()
+export const createMap = (newMapName, ownerUsername, userEmail, mapType) => {
+    return api.post(`/map/`, {
+        // SPECIFY THE PAYLOAD
+        name: newMapName,
+        ownerName: ownerUsername,
+        ownerEmail: userEmail,
+        mapType: mapType,
+        comments: [],
+        published: false,
+        publishedDate: null,
+        likes: [],
+        dislikes: [],
+        views: 0,
+    })
+}
+export const deleteMapById = (id) => api.delete(`/map/${id}`)
+export const getMapById = (id) => api.get(`/map/${id}`)
+export const getMapPairs = () => api.get(`/mapPairs/`)
+export const updateMapById = (id, map) => {
+    return api.put(`/map/${id}`, {
+        // SPECIFY THE PAYLOAD
+        map : map
+    })
+}
+export const updateUserFeedback = (id, map) => {
+    return api.put(`/maps/${id}`, {
+        // SPECIFY THE PAYLOAD
+        map : map
+    })
+}
+export const getPublishedLists = () => api.get('/maps/')
+
+const apis = {
+    createMap,
+    deleteMapById,
+    getMapById,
+    getMapPairs,
+    updateMapById,
+    updateUserFeedback,
+    getPublishedLists,
+}
+
+export default apis
