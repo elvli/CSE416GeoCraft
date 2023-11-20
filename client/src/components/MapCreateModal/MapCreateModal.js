5ce05768993f0b0769e4867bc6cdf12cf5c10b15
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-20 01:19:23 -0500

    Update MapCreateModal.js

--- client/src/components/MapCreateModal/MapCreateModal.js
+++ client/src/components/MapCreateModal/MapCreateModal.js
@@ -20,6 +20,7 @@ export default function MapCreateModal(props) {
       event.preventDefault();
       event.stopPropagation();
       const formData = new FormData(event.currentTarget);
+      console.log(formData.get('mapType'))
       store.createNewMap(
         formData.get("mapName"),
         formData.get("mapType")
@@ -43,18 +44,18 @@ export default function MapCreateModal(props) {
           <Modal.Body>
             <Form.Group>
               <Form.Label>Enter the Name of Your Map</Form.Label>
-              <Form.Control className="map-name" name="mapName" required type="text" placeholder="Map Name" />
+              <Form.Control className="map-name" name='mapName' required type="text" placeholder="Map Name" />
             </Form.Group>
             <br />
             <Form.Group>
               <Form.Label>Select Your Map Type</Form.Label>
-              <Form.Select name="mapType" required >
+              <Form.Select name='mapType' required >
                 <option value="">Select your map type</option>
-                <option value="1">Heat Map</option>
-                <option value="2">Point Map</option>
-                <option value="3">Proportional Symbols Map</option>
-                <option value="2">Choropleth Map</option>
-                <option value="3">Arrow Map</option>
+                <option value="Heat Map">Heat Map</option>
+                <option value="Point Map">Point Map</option>
+                <option value="Proportional Symbols Map">Proportional Symbols Map</option>
+                <option value="Choropleth Map">Choropleth Map</option>
+                <option value="Arrow Map">Arrow Map</option>
               </Form.Select>
             </Form.Group>
           </Modal.Body>
@@ -63,7 +64,7 @@ export default function MapCreateModal(props) {
               Close
             </Button>
             <Form.Group>
-              <Button variant="primary" type="submit" onClick={handleSubmit}>
+              <Button variant="primary" type="submit">
                 Create
               </Button>
             </Form.Group>
