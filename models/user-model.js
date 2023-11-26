79beb008918f5981412ddc5bc0dc79c5477c0f38
elvenli54 <elvenli54@gmail.com>
2023-11-25 23:35:43 -0500

    Made About Me required in user-model

--- models/user-model.js
+++ models/user-model.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     passwordHash: { type: String, required: true },
-    aboutMe: {type: String },
+    aboutMe: {type: String, required: true },
     maps: [{ type: ObjectId, ref: 'Maps' }],
   },
   { timestamps: true },
