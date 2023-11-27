c75f8f61a58d7e8400c9f65169efd88344f2219c
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-11-27 00:03:05 -0500

    Update MapCard.js

--- client/src/components/MapCard/MapCard.js
+++ client/src/components/MapCard/MapCard.js
@@ -167,7 +167,7 @@ export default function MapCard(props) {
       </Dropdown.Toggle>
       <Dropdown.Menu className='dropdown-menu'>
         <Dropdown.Item onClick={functions.handleFork} className='options-button-options'>Fork</Dropdown.Item>
-        <Dropdown.Item onClick={handleDelete} className='options-button-options'>Delete</Dropdown.Item>
+        <Dropdown.Item onClick={functions.handleDeleteMap} className='options-button-options'>Delete</Dropdown.Item>
         <Dropdown.Item className='options-button-options'>Rename</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
