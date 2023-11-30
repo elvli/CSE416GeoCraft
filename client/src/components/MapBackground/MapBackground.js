0a68309476e9312a546e341bfa43f71839008015
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-29 23:33:24 -0500

    Added region name popups

--- client/src/components/MapBackground/MapBackground.js
+++ client/src/components/MapBackground/MapBackground.js
@@ -2,8 +2,8 @@ import './MapBackground.scss';
 import React, { useRef, useEffect, useState, useContext } from 'react';
 import GlobalStoreContext from '../../store';
 import 'mapbox-gl/dist/mapbox-gl.css';
-// import rewind from "@mapbox/geojson-rewind";
 import mapboxgl from 'mapbox-gl';
+// import rewind from "@mapbox/geojson-rewind";
 // import usaGeoJSON from '../SampleGeoJSONs/USA_adm0-2.json';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWx2ZW5saTU0IiwiYSI6ImNsb3RiazljdTA3aXkycm1tZWUzYXNiMTkifQ.aknGR78_Aed8cL6MXu6KNA';
@@ -16,7 +16,12 @@ export default function MapBackground() {
   const [lng, setLng] = useState(12.7971);
   const [lat, setLat] = useState(41.8473);
   const [zoom, setZoom] = useState(5.43);
-  //   const [count, setCount] = useState(1);
+
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+    className: 'region-name-popup',
+  });
 
   useEffect(() => {
     if (map.current || typeof window === 'undefined') return;
@@ -80,9 +85,12 @@ export default function MapBackground() {
 
         if (hoveredRegion) {
           const regionId = hoveredRegion.properties.ID_1;
+          const regionName = hoveredRegion.properties.NAME_1;
 
           map.current.setFilter('italy-fill', ['==', 'ID_1', regionId]);
           map.current.setPaintProperty('italy-fill', 'fill-opacity', 1);
+
+          popup.setLngLat(e.lngLat).setHTML(`<p>${regionName}</p>`).addTo(map.current);
         }
       });
 
