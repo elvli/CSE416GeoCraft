a967d413b584a1e39dbef6b8b0ee49b2c06c003f
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-12-01 16:38:51 -0500

    Unique Edit URL

--- client/src/components/MapBackground/MapBackground.js
+++ client/src/components/MapBackground/MapBackground.js
@@ -17,12 +17,6 @@ export default function MapBackground() {
   const [lat, setLat] = useState(41.8473);
   const [zoom, setZoom] = useState(5.43);
 
-  const popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false,
-    className: 'region-name-popup',
-  });
-
   useEffect(() => {
     if (map.current || typeof window === 'undefined') return;
 
@@ -38,6 +32,7 @@ export default function MapBackground() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
+
     map.current.on('load', () => {
       map.current.addSource('map-source', {
         type: 'geojson',
@@ -79,6 +74,12 @@ export default function MapBackground() {
         filter: ['==', 'ID_1', ''], // Initially, no region is highlighted
       });
 
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+        className: 'region-name-popup',
+      });
+
       // Mousemove event to highlight the region under the cursor
       map.current.on('mousemove', 'italy-border-fill', (e) => {
         const hoveredRegion = e.features[0];
@@ -99,10 +100,10 @@ export default function MapBackground() {
         map.current.setFilter('italy-fill', ['==', 'ID_1', '']);
         map.current.setPaintProperty('italy-fill', 'fill-opacity', 0);
 
-        popup.remove(); 
+        popup.remove();
       });
     });
-  }, [lng, lat, zoom]);
+  }, [mapContainer, lng, lat, zoom]);
 
   return (
     <div>
