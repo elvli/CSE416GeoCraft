eadfb2edbbac4863919ce1b5b9d35c4b71573503
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-12-02 22:01:07 -0500

    Clicking map cards dynamically load geojsons

--- client/src/components/MapBackground/MapBackground.js
+++ client/src/components/MapBackground/MapBackground.js
@@ -3,36 +3,123 @@ import React, { useRef, useEffect, useState, useContext } from 'react';
 import GlobalStoreContext from '../../store';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
-// import rewind from "@mapbox/geojson-rewind";
-// import usaGeoJSON from '../SampleGeoJSONs/USA_adm0-2.json';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWx2ZW5saTU0IiwiYSI6ImNsb3RiazljdTA3aXkycm1tZWUzYXNiMTkifQ.aknGR78_Aed8cL6MXu6KNA';
-// const shp = require("shpjs");
 
 export default function MapBackground() {
   const { store } = useContext(GlobalStoreContext);
-  const mapContainer = store.container
+  const mapContainer = store.container;
   const map = useRef(null);
   const [lng, setLng] = useState(12.7971);
   const [lat, setLat] = useState(41.8473);
   const [zoom, setZoom] = useState(5.43);
 
+  async function generateMap(id, mapbox) {
+    if (mapbox.current || typeof window === 'undefined') return;
+
+    try {
+      mapbox.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/dark-v11',
+        center: [lng, lat],
+        zoom: zoom,
+      });
+
+      mapbox.current.on('move', () => {
+        setLng(mapbox.current.getCenter().lng.toFixed(4));
+        setLat(mapbox.current.getCenter().lat.toFixed(4));
+        setZoom(mapbox.current.getZoom().toFixed(2));
+      });
+
+      mapbox.current.on('load', () => {
+        mapbox.current.addSource('map-source', {
+          type: 'geojson',
+          data: null,
+        });
+
+        mapbox.current.addLayer({
+          id: 'geojson-border',
+          type: 'line',
+          source: 'map-source',
+          paint: {
+            'line-opacity': 1,
+            'line-color': '#FFFFFF',
+            'line-width': 1,
+          },
+        });
+
+        mapbox.current.addLayer({
+          id: 'geojson-border-fill',
+          type: 'fill',
+          source: 'map-source',
+          paint: {
+            'fill-opacity': 0.5,
+            'fill-color': '#FFFFFF'
+          }
+        });
+
+        mapbox.current.addLayer({
+          id: 'italy-fill',
+          type: 'fill',
+          source: 'map-source',
+          paint: {
+            'fill-opacity': 0,
+            'fill-color': '#FF0000'
+          },
+          filter: ['==', 'ID_1', ''],
+        });
+
+        const popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false,
+          className: 'region-name-popup',
+        });
+
+        mapbox.current.on('mousemove', 'geojson-border-fill', (e) => {
+          const hoveredRegion = e.features[0];
+
+          if (hoveredRegion) {
+            const regionId = hoveredRegion.properties.ID_1;
+            const regionName = hoveredRegion.properties.NAME_1;
+
+            mapbox.current.setFilter('italy-fill', ['==', 'ID_1', regionId]);
+            mapbox.current.setPaintProperty('italy-fill', 'fill-opacity', 1);
+
+            popup.setLngLat(e.lngLat).setHTML(`<p>${regionName}</p>`).addTo(mapbox.current);
+          }
+        });
+
+        mapbox.current.on('mouseleave', 'geojson-border-fill', () => {
+          mapbox.current.setFilter('italy-fill', ['==', 'ID_1', '']);
+          mapbox.current.setPaintProperty('italy-fill', 'fill-opacity', 0);
+
+          popup.remove();
+        });
+      });
+    } catch (error) {
+      console.error('Error generating map:', error);
+    }
+  }
+
   useEffect(() => {
-    if (map.current || typeof window === 'undefined') return;
+    generateMap(store.currentList ? store.currentList._id : null, map);
+  }, [store.currentList]);
 
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/dark-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    });
+  useEffect(() => {
+    const updateMapData = async () => {
+      try {
+        if (store.currentList) {
+          const mapData = await store.getMapDataById(store.currentList._id);
+          const geoJSON = mapData ? mapData.GeoJson : 'https://raw.githubusercontent.com/elvli/GeoJSONFiles/main/ITA_adm1-2.json';
+          map.current.getSource('map-source').setData(geoJSON);
+        }
+      } catch (error) {
+        console.error('Error updating map data:', error);
+      }
+    };
 
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  }, [mapContainer, lng, lat, zoom]);
+    updateMapData();
+  }, [store.currentList]);
 
   return (
     <div>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -278,7 +278,7 @@ function GlobalStoreContextProvider(props) {
       }
       if (!location.pathname.includes('/profile')) {
         // generateMap if not on a /profile URL
-        generateMap(id, mapbox)
+        // generateMap(id, mapbox)
       }
     }
     asyncSetCurrentList(id);
@@ -302,114 +302,96 @@ function GlobalStoreContextProvider(props) {
     return getMapDataById(id);
   };
 
-  // store.getGeoJSONById = async function (id) {
-  //   async function getGeoJSONById(id) {
-  //     try {
-  //       let response = await api.getGeoJSONById(id);
-  //       if (response.data.success) {
-  //         console.log('STORE: ' + JSON.stringify(response.data.mapData));
-  //         return response.data.mapData;
-  //       } else {
-  //         console.log('getGeoJSONById has thrown an error');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching map data:', error);
-  //     }
+  // async function generateMap(id, mapbox) {
+  //   if (mapbox.current || typeof window === 'undefined') return;
+
+  //   try {
+  //     const mapData = await store.getMapDataById(id);
+  //     let geoJSON = mapData ? mapData.GeoJson : 'https://raw.githubusercontent.com/elvli/GeoJSONFiles/main/ITA_adm1-2.json';
+
+  //     mapbox.current = new mapboxgl.Map({
+  //       container: store.container.current,
+  //       style: 'mapbox://styles/mapbox/dark-v11',
+  //       center: [lng, lat],
+  //       zoom: zoom,
+  //     });
+
+  //     mapbox.current.on('move', () => {
+  //       setLng(mapbox.current.getCenter().lng.toFixed(4));
+  //       setLat(mapbox.current.getCenter().lat.toFixed(4));
+  //       setZoom(mapbox.current.getZoom().toFixed(2));
+  //     });
+
+  //     mapbox.current.on('load', () => {
+  //       mapbox.current.addSource('map-source', {
+  //         type: 'geojson',
+  //         data: geoJSON,
+  //       });
+
+  //       // This renders the border of the GeoJSON
+  //       mapbox.current.addLayer({
+  //         id: 'italy-border',
+  //         type: 'line',
+  //         source: 'map-source',
+  //         paint: {
+  //           'line-opacity': 1,
+  //           'line-color': '#FFFFFF',
+  //           'line-width': 1,
+  //         },
+  //       });
+
+  //       // This fills in the regions of the GeoJSON
+  //       mapbox.current.addLayer({
+  //         id: 'italy-border-fill',
+  //         type: 'fill',
+  //         source: 'map-source',
+  //         paint: {
+  //           'fill-opacity': 0.5,
+  //           'fill-color': '#FFFFFF'
+  //         }
+  //       });
+
+  //       // This layer fills in the region
+  //       mapbox.current.addLayer({
+  //         id: 'italy-fill',
+  //         type: 'fill',
+  //         source: 'map-source',
+  //         paint: {
+  //           'fill-opacity': 0,
+  //           'fill-color': '#FF0000'
+  //         },
+  //         filter: ['==', 'ID_1', ''], // Initially, no region is highlighted
+  //       });
+
+  //       // Mousemove event to highlight the region under the cursor
+  //       mapbox.current.on('mousemove', 'italy-border-fill', (e) => {
+  //         const hoveredRegion = e.features[0];
+
+  //         if (hoveredRegion) {
+  //           const regionId = hoveredRegion.properties.ID_1;
+  //           const regionName = hoveredRegion.properties.NAME_1;
+
+  //           mapbox.current.setFilter('italy-fill', ['==', 'ID_1', regionId]);
+  //           mapbox.current.setPaintProperty('italy-fill', 'fill-opacity', 1);
+
+  //           popup.setLngLat(e.lngLat).setHTML(`<p>${regionName}</p>`).addTo(mapbox.current);
+  //         }
+  //       });
+
+  //       // Reset the filter and opacity when the mouse leaves the layer
+  //       mapbox.current.on('mouseleave', 'italy-border-fill', () => {
+  //         mapbox.current.setFilter('italy-fill', ['==', 'ID_1', '']);
+  //         mapbox.current.setPaintProperty('italy-fill', 'fill-opacity', 0);
+
+  //         popup.remove();
+  //       });
+
+  //     });
   //   }
-
-  //   return getGeoJSONById(id);
-  // };
-
-  async function generateMap(id, mapbox) {
-    if (mapbox.current || typeof window === 'undefined') return;
-
-    try {
-      const mapData = await store.getMapDataById(id);
-      let geoJSON = mapData ? mapData.GeoJson : 'https://raw.githubusercontent.com/elvli/GeoJSONFiles/main/ITA_adm1-2.json';
-
-      mapbox.current = new mapboxgl.Map({
-        container: store.container.current,
-        style: 'mapbox://styles/mapbox/dark-v11',
-        center: [lng, lat],
-        zoom: zoom,
-      });
-
-      mapbox.current.on('move', () => {
-        setLng(mapbox.current.getCenter().lng.toFixed(4));
-        setLat(mapbox.current.getCenter().lat.toFixed(4));
-        setZoom(mapbox.current.getZoom().toFixed(2));
-      });
-
-      mapbox.current.on('load', () => {
-        mapbox.current.addSource('map-source', {
-          type: 'geojson',
-          data: geoJSON,
-        });
-
-        // This renders the border of the GeoJSON
-        mapbox.current.addLayer({
-          id: 'italy-border',
-          type: 'line',
-          source: 'map-source',
-          paint: {
-            'line-opacity': 1,
-            'line-color': '#FFFFFF',
-            'line-width': 1,
-          },
-        });
-
-        // This fills in the regions of the GeoJSON
-        mapbox.current.addLayer({
-          id: 'italy-border-fill',
-          type: 'fill',
-          source: 'map-source',
-          paint: {
-            'fill-opacity': 0.5,
-            'fill-color': '#FFFFFF'
-          }
-        });
-
-        // This layer fills in the region
-        mapbox.current.addLayer({
-          id: 'italy-fill',
-          type: 'fill',
-          source: 'map-source',
-          paint: {
-            'fill-opacity': 0,
-            'fill-color': '#FF0000'
-          },
-          filter: ['==', 'ID_1', ''], // Initially, no region is highlighted
-        });
-
-        // Mousemove event to highlight the region under the cursor
-        mapbox.current.on('mousemove', 'italy-border-fill', (e) => {
-          const hoveredRegion = e.features[0];
-
-          if (hoveredRegion) {
-            const regionId = hoveredRegion.properties.ID_1;
-            const regionName = hoveredRegion.properties.NAME_1;
-
-            mapbox.current.setFilter('italy-fill', ['==', 'ID_1', regionId]);
-            mapbox.current.setPaintProperty('italy-fill', 'fill-opacity', 1);
-
-            popup.setLngLat(e.lngLat).setHTML(`<p>${regionName}</p>`).addTo(mapbox.current);
-          }
-        });
-
-        // Reset the filter and opacity when the mouse leaves the layer
-        mapbox.current.on('mouseleave', 'italy-border-fill', () => {
-          mapbox.current.setFilter('italy-fill', ['==', 'ID_1', '']);
-          mapbox.current.setPaintProperty('italy-fill', 'fill-opacity', 0);
-
-          popup.remove();
-        });
-
-      });
-    }
-    catch (error) {
-      console.error('Error generating map:', error);
-    }
-  }
+  //   catch (error) {
+  //     console.error('Error generating map:', error);
+  //   }
+  // }
 
 
   return (
