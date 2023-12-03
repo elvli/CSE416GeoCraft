c7ec08e514cdda405647393b24b243ccf6bbe137
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-12-02 22:06:07 -0500

    Removed Create.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
-import './App.css';
-// import baseUrl from "./baseUrl";
-// import axios from "axios";
-import { React} from "react";
-import { Route, Routes} from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { AuthContextProvider } from './auth';
 import { GlobalStoreContextProvider } from './store';
-import { LoginScreen, SignUpScreen, HomeScreen, PasswordReset, VerifyScreen, ConfirmScreen, EditScreen, ProfilePage } from './components'
+import {
+  LoginScreen,
+  SignUpScreen,
+  HomeScreen,
+  PasswordReset,
+  VerifyScreen,
+  ConfirmScreen,
+  EditScreen,
+  ProfilePage
+} from './components';
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <AuthContextProvider>
         <GlobalStoreContextProvider>
-            <Routes>
-              <Route index element={<HomeScreen />} />
-              <Route path="sign-up/" element={<SignUpScreen />} />
-              <Route path="login/" element={<LoginScreen />} />
-              <Route path="password-reset/" element={<PasswordReset />} />
-              <Route path="verify/" element={<VerifyScreen />} />
-              <Route path="confirm/" element={<ConfirmScreen />} />
-              <Route path="edit/:mapId" element={<EditScreen />} />
-              <Route path="profile/:username" element={<ProfilePage />} />
-            </Routes>
+          <Routes>
+            <Route index element={<HomeScreen />} />
+            <Route path="sign-up/" element={<SignUpScreen />} />
+            <Route path="login/" element={<LoginScreen />} />
+            <Route path="password-reset/" element={<PasswordReset />} />
+            <Route path="verify/" element={<VerifyScreen />} />
+            <Route path="confirm/" element={<ConfirmScreen />} />
+            <Route path="edit/:mapId" element={<EditScreen />} />
+            <Route path="profile/:username" element={<ProfilePage />} />
+          </Routes>
         </GlobalStoreContextProvider>
       </AuthContextProvider>
-      
+
     </div>
   );
 }
-/*<Routes>
-        <Route index element={<HomeScreen />} />
-        <Route path="signup/" element={<SignUpScreen />} />
-        <Route path="login/" element={<LoginScreen />} />
-        <Route path="password-reset/" element={<PasswordReset />} />
-      </Routes>*/
+
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import Create from "./Create";
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import { BrowserRouter } from "react-router-dom";
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
