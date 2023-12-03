c029200c41faf931c317515f9f8e443e231abdcc
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-12-03 02:54:53 -0500

    Trying Azure Backend

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://geocraftmapsbackend.onrender.com";
+// const baseUrl = "https://geocraftmapsbackend.onrender.com";
 // const baseUrl = "http://localhost:3001";
+const baseUrl = "https://geocraftbackend.azurewebsites.net"
 
 export default baseUrl;
