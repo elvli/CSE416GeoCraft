032b0393eff0a997d4953457d94a02e5a5b50c59
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-12-02 20:52:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use(express.json({ limit: '20mb' }));
 app.use(cookieParser())
+app.use(bodyParser.json({ limit: '50mb' }));
 
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', baseUrl);
