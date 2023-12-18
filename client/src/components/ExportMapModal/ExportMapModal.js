ebc0f0cee9ae8aab7f1cce5d2aab80897d3c7a09
bshi111 <99460869+bshi111@users.noreply.github.com>
2023-12-17 22:08:43 -0500

    Fixed MapBackground and added export map functionality

--- client/src/components/ExportMapModal/ExportMapModal.js
+++ client/src/components/ExportMapModal/ExportMapModal.js
@@ -3,37 +3,94 @@ import "./ExportMapModal.scss";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import GlobalStoreContext from "../../store";
+import { useState, useContext, useRef } from "react";
 
 export default function ExportMapModal(props) {
+  const downloadLinkRef = useRef(null);
+  const [jsonData, setJsonData] = useState('');
   const { exportMapShow, handleExportMapClose } = props
-
+  const [validated, setValidated] = useState(false);
+  const { store } = useContext(GlobalStoreContext);
   const handleSubmit = (event) => {
-    handleExportMapClose(event)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    }
+    else {
+      event.preventDefault();
+      event.stopPropagation();
+      const formData = new FormData(event.currentTarget);
+      console.log(formData.get('mapType'))
+      var downloadType = formData.get("mapType")
+      if(downloadType == 'json') {
+        downloadJson()
+      }
+      else if(downloadType == 'jpeg') {
+        downloadPic(2)
+      }
+      else {
+        downloadPic(1)
+      }
+        
+      handleExportMapClose(event)
+    }
   };
 
   const handleClosing = (event) => {
     handleExportMapClose(event)
   };
+  const downloadJson = async () => {
+    const mapId = store.currentList._id
+    const data = await store.getMapDataById(mapId)
+    const json = JSON.stringify(data);
+    setJsonData(json);
 
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    downloadLinkRef.current.href = url;
+    var string = store.currentList.name
+    downloadLinkRef.current.download = string.concat('.json');
+    downloadLinkRef.current.click();
+
+    URL.revokeObjectURL(url);
+  };
+  const downloadPic = async (arg) => {
+    const rewait = await store.setPrint(arg)
+  }
   return (
     <div>
       <Modal centered show={exportMapShow} onHide={handleExportMapClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Export Map?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Are you sure you want to export this map?</p>
-        </Modal.Body>
-        <Modal.Footer className="d-flex justify-content-center">
-          <Button variant="secondary" onClick={handleClosing}>
-            Close
-          </Button>
-          <Form.Group>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-              Confirm
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Export Map?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Are you sure you want to export this map?</p>
+            <Form.Group>
+              <Form.Label>Export as</Form.Label>
+              <Form.Select name='mapType' required >
+                <option value="">Select export type</option>
+                <option value="json">JSON</option>
+                <option value="jpeg">JPEG</option>
+                <option value="png">PNG</option>
+              </Form.Select>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer className="d-flex justify-content-center">
+            <Button variant="secondary" onClick={handleClosing}>
+              Close
             </Button>
-          </Form.Group>
-        </Modal.Footer>
+            <Form.Group>
+              <Button variant="primary" type="submit">
+                Confirm
+              </Button>
+            </Form.Group>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </div>
   )
