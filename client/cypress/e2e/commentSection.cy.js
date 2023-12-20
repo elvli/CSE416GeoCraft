e07dad2aea43149d68cf0468c13b1d8d426ce523
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-12-20 03:57:44 -0500

    Added some more cypress tests

--- client/cypress/e2e/commentSection.cy.js
+++ client/cypress/e2e/commentSection.cy.js
@@ -1,36 +1,37 @@
 describe('Comment Section', () => {
-    beforeEach(() => {
-      cy.viewport(1200, 800);
-      cy.visit('https://geocraftmaps.azurewebsites.net/');
-      cy.get('.card.map-card').eq(0).click()
-    });
-    it('should collapse when the button is clicked', () => {
-      });
-
-      it('should check if the comment input is disabled', () => {
-        // Check if the comment input is disabled before attempting to type
-        cy.get('.form-control').eq(1).should('be.disabled');
-      });
-  
-    it('should type into the comment input', () => {
-      cy.on('uncaught:exception', (err, runnable) => {
-        // Returning false here prevents Cypress from failing the test
-        return false;
-      });
-      // Perform login operation
-      cy.visit('https://geocraftmaps.azurewebsites.net/login');
-      // Input valid login credentials
-      cy.get('[name="email"]').type('elvenli54@gmail.com');
-      cy.get('[name="password"]').type('123123123');
-
-      // Submit the form
-      cy.get('.form-button').click();
-
-      cy.get('.card.map-card').eq(0).click()
-      const commentText = 'Testing Cypress';
-      // Find the comment input, type text, and check the value
-      cy.get('.form-control').eq(1).type(commentText);    
-      cy.get('.form-control').eq(1).should('have.value', commentText);
-    });
+  beforeEach(() => {
+    cy.viewport(1200, 800);
+    cy.visit('https://geocraftmaps.azurewebsites.net/');
+    cy.get('.card.map-card').eq(0).click()
   });
-  
+  it('should collapse when the button is clicked', () => {
+  });
+
+  it('should check if the comment input is disabled', () => {
+    // Check if the comment input is disabled before attempting to type
+    cy.get('.app-banner .home-button').click();
+    cy.get('.map-card .card-header').eq(0).click();
+    cy.get('.form-control').should('be.disabled');
+  });
+
+  it('should type into the comment input', () => {
+    cy.on('uncaught:exception', (err, runnable) => {
+      // Returning false here prevents Cypress from failing the test
+      return false;
+    });
+    // Perform login operation
+    cy.visit('https://geocraftmaps.azurewebsites.net/login');
+    // Input valid login credentials
+    cy.get('[name="email"]').type('elvenli54@gmail.com');
+    cy.get('[name="password"]').type('123123123');
+
+    // Submit the form
+    cy.get('.form-button').click();
+
+    cy.get('.card.map-card').eq(0).click()
+    const commentText = 'Testing Cypress';
+    // Find the comment input, type text, and check the value
+    cy.get('.form-control').eq(1).type(commentText);
+    cy.get('.form-control').eq(1).should('have.value', commentText);
+  });
+});
--- client/cypress/e2e/editSideBar.cy.js
+++ client/cypress/e2e/editSideBar.cy.js
@@ -1,71 +1,75 @@
 describe('EditSideBar Component', () => {
-    before(() => {
-      // Perform login operation
-      cy.visit('https://geocraftmaps.azurewebsites.net/login');
-      // Input valid login credentials
-      cy.get('[name="email"]').type('elvenli54@gmail.com');
-      cy.get('[name="password"]').type('123123123');
+  before(() => {
+    // Perform login operation
+    cy.visit('https://geocraftmaps.azurewebsites.net/login');
+    // Input valid login credentials
+    cy.get('[name="email"]').type('elvenli54@gmail.com');
+    cy.get('[name="password"]').type('123123123');
 
-      // Submit the form
-      cy.get('.form-button').click();
-    });
-
-    it('should allow you to look at only your owned maps', () => {
-      cy.get('.btn.btn-light.my-maps-btn.btn.btn-primary').click();
-  
-    });
-  
-    it('should edit header on double click', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('.accordion-button.collapsed').first().click();
-      cy.get('th').first().dblclick();
-    });
-  
-    it('should download JSON on button click', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-    cy.get('.accordion-button.collapsed').last().click();
-      cy.get('button:contains("Download JSON")').click();
-    });
-
-    it('should generate a heatmap', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#my_file_input').click();
-    });
-  
-    it('should open SaveAndExitModal on close button click', () => {
-      cy.viewport(1200, 800);
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-close-button').click();
-      cy.get('.modal-content').should('be.visible');
-    });
-
-    it('should toggle sidebar', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-left-tool .edit-button').eq(0).click();
-    });
-  
-    it('should handle save', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-left-tool .edit-button').eq(1).click();
-    });
-  
-    it('should handle undo', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-left-tool .edit-button').eq(2).click();
-    });
-  
-    it('should handle redo', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-left-tool .edit-button').eq(3).click();
-    });
-  
-    it('should show map name modal', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-left-tool .edit-button').eq(4).click();
-    });
-  
-    it('should show close modal', () => {
-      cy.visit('https://geocraftmaps.azurewebsites.net/edit/658266f6d7159b87052f2c65');
-      cy.get('#edit-left-tool #edit-close-button').click();
-    });
+    // Submit the form
+    cy.get('.form-button').click();
   });
+
+  it('should allow you to look at only your owned maps', () => {
+    cy.get('.btn.btn-light.my-maps-btn.btn.btn-primary').click();
+
+  });
+
+  it('should download JSON on button click', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('.accordion-button.collapsed').last().click();
+    cy.get('button:contains("Download JSON")').click();
+  });
+
+  it('should generate a heatmap', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#my_file_input').click();
+  });
+
+  it('should open SaveAndExitModal on close button click', () => {
+    cy.viewport(1200, 800);
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-close-button').click();
+    cy.get('.modal-content').should('be.visible');
+  });
+
+  it('should toggle sidebar', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool .edit-button').eq(0).click();
+  });
+
+  it('should handle save', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool .edit-button').eq(1).click();
+  });
+
+  it('should not undo', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool .edit-button').eq(2).should('not.be.visible');
+  });
+
+  it('should not redo', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool .edit-button').eq(3).should('not.be.visible');
+  });
+
+  it('should show map name modal', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool .edit-button').eq(4).click();
+  });
+
+  it('should show close modal', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool #edit-close-button').click();
+  });
+
+  it('should show publish modal', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('#edit-left-tool .edit-button').eq(5).click();
+  });
+
+  it('should go to home page', () => {
+    cy.visit('https://geocraftmaps.azurewebsites.net/edit/6582a76a8eebaa5e249b13a2');
+    cy.get('.app-banner .home-button').click();
+  });
+});
